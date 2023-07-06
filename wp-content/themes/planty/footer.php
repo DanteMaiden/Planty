d78223f0882d5dc1013d413a24b0a2450086697b
DanteMaiden <david.bartholome70@hotmail.com>
2023-07-06 13:01:39 +0200

    add pages style

---
+++ wp-content/themes/planty/footer.php
@@ -0,0 +1,14 @@
+</main>
+</div>
+
+<!-- Visu du footer -->
+<footer id="footer" role="contentinfo">
+    <div id="mentions-legales">
+        <a href="#">Mentions Légales</a>
+    </div>
+</footer>
+
+</div>
+<?php wp_footer(); ?>
+</body>
+</html>

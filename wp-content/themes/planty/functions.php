d6678d466a9c0567505e15c5e884472517dad97f
DanteMaiden <david.bartholome70@hotmail.com>
2023-07-06 13:54:48 +0200

    add header style

--- wp-content/themes/planty/functions.php
+++ wp-content/themes/planty/functions.php
@@ -7,3 +7,23 @@ function theme_enqueue_styles() {
  wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 
 }
+
+
+/**
+** code widget logo
+**/
+
+function header_widgets_init() {
+ 
+    register_sidebar( array(
+   
+    'name' => 'Logo',
+    'id' => 'logo-widget-area',
+    'before_widget' => '<div class="nwa-widget">',
+    'after_widget' => '</div>',
+    'before_title' => '<h2 class="nwa-title">',
+    'after_title' => '</h2>',
+    ) );
+   }
+   
+   add_action( 'widgets_init', 'header_widgets_init' );
--- wp-content/themes/planty/header.php
+++ wp-content/themes/planty/header.php
@@ -8,21 +8,28 @@
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
 <div id="wrapper" class="hfeed">
-<header id="header" role="banner">
-<div id="branding">
-<div id="site-title" itemprop="publisher" itemscope itemtype="https://schema.org/Organization">
-<?php
-if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo '<h1>'; }
-echo '<a href="' . esc_url( home_url( '/' ) ) . '" title="' . esc_attr( get_bloginfo( 'name' ) ) . '" rel="home" itemprop="url"><span itemprop="name">' . esc_html( get_bloginfo( 'name' ) ) . '</span></a>';
-if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo '</h1>'; }
-?>
-</div>
-<div id="site-description"<?php if ( !is_single() ) { echo ' itemprop="description"'; } ?>><?php bloginfo( 'description' ); ?></div>
-</div>
-<nav id="menu" role="navigation" itemscope itemtype="https://schema.org/SiteNavigationElement">
-<?php wp_nav_menu( array( 'theme_location' => 'main-menu', 'link_before' => '<span itemprop="name">', 'link_after' => '</span>' ) ); ?>
-<div id="search"><?php get_search_form(); ?></div>
-</nav>
-</header>
-<div id="container">
-<main id="content" role="main">
+    <header id="header" role="banner">
+
+        <!-- logo / titre -->
+        <div id="branding">
+            <div id="site-title" itemprop="publisher" itemscope itemtype="https://schema.org/Organization">
+                 <!-- ajout de ma nouvelle widget area -->
+                <?php if ( is_active_sidebar( 'logo-widget-area' ) ) : ?>
+                    <div id="header-widget-area" class="logo-header-widget widget-area" role="complementary">
+                        <?php dynamic_sidebar( 'logo-widget-area' ); ?>
+                    </div>
+                <?php endif; ?>
+                <!-- fin nouvelle widget area -->
+            </div>
+        </div>
+
+        <!-- navigation / menu -->
+        <nav id="menu" role="navigation" itemscope itemtype="https://schema.org/SiteNavigationElement">
+           <a href="#">Nous rencontrer</a>
+           <a href="#">Admin</a>
+           <a href="#" class="bouton-rose">Commander</a>
+        </nav>
+
+    </header>
+    <div id="container">
+        <main id="content" role="main">
